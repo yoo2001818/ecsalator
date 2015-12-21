829b3a6b4a29ea3cb17e3ce76d46b2a675bfa51a
Duknam Yoo <yoo2001818@gmail.com>
2015-12-21 22:22:58 +0900

    Create interface for event queue

---
+++ src/eventQueue.js
@@ -0,0 +1,35 @@
+/* @flow */
+
+// Queues the events and emits them once after the action has processed.
+
+/* eslint-disable no-unused-vars */
+
+export default class EventQueue {
+  constructor() {
+
+  }
+
+  observeEntry(id: number, listener: Function): void {
+
+  }
+
+  unobserveEntry(id: number, listener: Function): void {
+
+  }
+
+  observeComponent(name: string, listener: Function): void {
+
+  }
+
+  unobserveComponent(name: string, listener: Function): void {
+
+  }
+
+  push(entity: number, component: string, previous: Object): void {
+
+  }
+
+  notify(): void {
+
+  }
+}
