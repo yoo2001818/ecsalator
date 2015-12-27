0e0264cf2c5dc5b90405c9082ec116a27409c76b
Duknam Yoo <yoo2001818@gmail.com>
2015-12-27 23:33:57 +0900

    Implement entity tests

--- test/entity.js
+++ test/entity.js
@@ -1,27 +1,135 @@
 import expect from 'expect';
 
+import Engine from '../src/engine';
 import Entity from '../src/entity';
 
 describe('Entity', () => {
+  let engine, entity;
+  // Action create functions.
+  const set = (entity, key, value) => ({
+    type: 'set',
+    payload: {
+      entity, key, value
+    },
+    meta: {}
+  });
+  const remove = (entity, key) => ({
+    type: 'remove',
+    payload: {
+      entity, key
+    },
+    meta: {}
+  });
+  // Since Entity works with the engine, we need to initialize the engine too.
+  beforeEach('initialize engine with entities', () => {
+    engine = new Engine([], [
+      // We can't directly mutate the engine; we need to use a system
+      // to mutate it.
+      (engine, action) => {
+        const { type, payload } = action;
+        if (type === 'set') {
+          const { entity, key, value } = payload;
+          entity.set(key, value);
+        }
+        if (type === 'remove') {
+          const { entity, key } = payload;
+          entity.remove(key);
+        }
+      }
+    ], ['test'], {
+      id: {
+        1: 1
+      },
+      test: {
+        1: 'sleepy'
+      }
+    });
+    entity = engine.get(1);
+  });
   describe('#get', () => {
-    it('should return a component');
-    it('should throw error when component is invalid');
-    it('should throw error when entity is invalid');
+    it('should return a component', () => {
+      expect(entity.get('test')).toBe('sleepy');
+    });
+    it('should throw error when component is invalid', () => {
+      expect(() => entity.get('never')).toThrow();
+    });
+    it('should throw error when entity is invalid', () => {
+      entity = new Entity(engine, 32767);
+      expect(() => entity.get('test')).toThrow();
+    });
   });
   describe('#set', () => {
-    it('should set a component');
-    it('should prevent overriding id component');
-    it('should throw error when component is invalid');
-    it('should throw error when entity is invalid');
-    it('should emit an entity event');
-    it('should emit a component event');
+    it('should set a component', () => {
+      engine.dispatch(set(entity, 'test', '2spooky4me'));
+      expect(entity.get('test')).toBe('2spooky4me');
+    });
+    it('should prevent overriding id component', () => {
+      expect(() => engine.dispatch(set(entity, 'id', 'nope'))).toThrow();
+    });
+    it('should throw error when component is invalid', () => {
+      expect(() => engine.dispatch(set(entity, 'tera', 'byte'))).toThrow();
+    });
+    it('should throw error when entity is invalid', () => {
+      entity = new Entity(engine, 32767);
+      expect(() => engine.dispatch(set(entity, 'test', 'nevermind'))).toThrow();
+    });
+    it('should emit an event', () => {
+      // Here, Set up the observer to verify if the event has succeeded.
+      let count = 0;
+      engine.observe('test', () => count++);
+      entity.observe(() => count++);
+      engine.dispatch(set(entity, 'test', 'probably'));
+      expect(count).toBe(2);
+    });
+    it('should have previous state in events', () => {
+      engine.observe('test', event => {
+        expect(event.type).toBe('component');
+        expect(event.key).toBe('test');
+        expect(event.values[1]).toBe('sleepy');
+      });
+      entity.observe(event => {
+        expect(event.type).toBe('entity');
+        expect(event.key).toBe(1);
+        expect(event.values['test']).toBe('sleepy');
+      });
+      engine.dispatch(set(entity, 'test', 'yes'));
+    });
   });
   describe('#remove', () => {
-    it('should remove a component');
-    it('should prevent removing id component');
-    it('should throw error when component is invalid');
-    it('should throw error when entity is invalid');
-    it('should emit an entity event');
-    it('should emit a component event');
+    it('should remove a component', () => {
+      engine.dispatch(remove(entity, 'test'));
+      expect(entity.get('test')).toEqual(undefined);
+    });
+    it('should prevent removing id component', () => {
+      expect(() => engine.dispatch(remove(entity, 'id'))).toThrow();
+    });
+    it('should throw error when component is invalid', () => {
+      expect(() => engine.dispatch(remove(entity, 'giga'))).toThrow();
+    });
+    it('should throw error when entity is invalid', () => {
+      entity = new Entity(engine, 32767);
+      expect(() => engine.dispatch(remove(entity, 'test'))).toThrow();
+    });
+    it('should emit an event', () => {
+      // Here, Set up the observer to verify if the event has succeeded.
+      let count = 0;
+      engine.observe('test', () => count++);
+      entity.observe(() => count++);
+      engine.dispatch(remove(entity, 'test'));
+      expect(count).toBe(2);
+    });
+    it('should have previous state in events', () => {
+      engine.observe('test', event => {
+        expect(event.type).toBe('component');
+        expect(event.key).toBe('test');
+        expect(event.values[1]).toBe('sleepy');
+      });
+      entity.observe(event => {
+        expect(event.type).toBe('entity');
+        expect(event.key).toBe(1);
+        expect(event.values['test']).toBe('sleepy');
+      });
+      engine.dispatch(remove(entity, 'test'));
+    });
   });
 });
