2839a6ea2b0043a5b18eb5ba257b7e2c1c63564f
Duknam Yoo <yoo2001818@gmail.com>
2016-01-05 00:03:25 +0900

    Cleanup old code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 /* @flow */
 
-import Engine from './engine';
-import Entity from './entity';
-import EventQueue from './eventQueue';
-
-export default Engine;
-export { Engine, Entity, EventQueue };
+// Do nothing yet
