e50a672977246845a721d6923ce4db71e563ffa1
Duknam Yoo <yoo2001818@gmail.com>
2016-02-27 22:27:38 +0900

    Add basic variables to state class

--- src/ecs/state.js
+++ src/ecs/state.js
@@ -1,15 +1,26 @@
 import Store from '../store';
+
+// A placeholder until the entity class is complete
+type Entity = Object;
+
 // An Entity-Component State implementation
 export default class State {
-  components: any;
-  componentConstructors: any;
-  componentList: any;
-  entities: any;
+  components: { [key: string]: Map<number, Object> };
+  componentList: Array<string>;
+  entities: Map<number, Entity>;
+  globals: Object;
   store: ?Store;
-  constructor() {
-
+  constructor(componentList: Array<string>) {
+    this.componentList = componentList;
+    this.components = {};
+    componentList.forEach(name => this.components[name] = new Map());
+    this.entities = new Map();
+    this.globals = {};
   }
   onMount(store: Store) {
     this.store = store;
+  }
+  get(id: number): ?Entity {
+    return this.entities.get(id);
   }
 }
